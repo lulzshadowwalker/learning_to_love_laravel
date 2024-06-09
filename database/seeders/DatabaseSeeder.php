7c1b8a8bfbd998c0f6f19ad36e2c0c3e18ab0566
lulzshadowwalker <lulzshadowwalker@gmail.com>
2024-06-09 23:57:46 +0300

    db migrations and seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Image;
+use App\Models\Product;
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Eloquent\Factories\Sequence;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,11 +15,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+
+        Product::factory(4)
+            ->hasVariants(5)
+            ->has(Image::factory()->sequence(fn (Sequence $sequence) => ['featured' => $sequence->index === 0]))
+            ->create();
     }
 }
